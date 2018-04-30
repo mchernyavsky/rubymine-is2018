62fdad5d8c6b6817d42828199af2f6b4318048d1
mchernyavsky <chemike47@gmail.com>
2018-04-30 04:11:56 +0300

    Add task 4

--- src/main/kotlin/com/jetbrains/python/inspection/PyConstantExpression.kt
+++ src/main/kotlin/com/jetbrains/python/inspection/PyConstantExpression.kt
@@ -29,82 +29,86 @@ class PyConstantExpression : PyInspection() {
         }
 
         private fun processIfPart(pyIfPart: PyIfPart) {
-            val condition = pyIfPart.condition
-            condition?.bool?.let {
+            val condition = pyIfPart.condition ?: return
+            condition.bool?.let {
                 registerProblem(condition, "The condition is always $it")
+                return
+            }
+            if (!condition.equation.canSolve()) {
+                registerProblem(condition, "The condition is always false")
+            }
+        }
+    }
+}
+
+internal val PyExpression.bool: Boolean?
+    get() = when (this) {
+        is PyBoolLiteralExpression -> value
+        is PyBinaryExpression -> bool
+        is PyPrefixExpression -> bool
+        is PyParenthesizedExpression -> containedExpression?.bool
+        else -> null
+    }
+
+internal val PyExpression.bigInt: BigInteger?
+    get() = when (this) {
+        is PyNumericLiteralExpression -> this.bigIntegerValue
+        is PyBinaryExpression -> bigInt
+        is PyParenthesizedExpression -> containedExpression?.bigInt
+        else -> null
+    }
+
+private val PyBinaryExpression.bool: Boolean?
+    get() {
+        val leftBigInt = leftExpression?.bigInt
+        val rightBigInt = rightExpression?.bigInt
+        if (leftBigInt != null && rightBigInt != null) {
+            return when (operator) {
+                PyTokenTypes.LT -> leftBigInt < rightBigInt
+                PyTokenTypes.GT -> leftBigInt > rightBigInt
+                PyTokenTypes.LE -> leftBigInt <= rightBigInt
+                PyTokenTypes.GE -> leftBigInt >= rightBigInt
+                PyTokenTypes.EQEQ -> leftBigInt == rightBigInt
+                PyTokenTypes.NE -> leftBigInt != rightBigInt
+                else -> null
             }
         }
 
-        private val PyExpression.bool: Boolean?
-            get() = when (this) {
-                is PyBoolLiteralExpression -> value
-                is PyBinaryExpression -> bool
-                is PyPrefixExpression -> bool
-                is PyParenthesizedExpression -> containedExpression?.bool
+        val leftBool = leftExpression?.bool
+        val rightBool = rightExpression?.bool
+        if (leftBool != null && rightBool != null) {
+            return when (operator) {
+                PyTokenTypes.AND_KEYWORD -> leftBool && rightBool
+                PyTokenTypes.OR_KEYWORD -> leftBool || rightBool
+                PyTokenTypes.EQEQ -> leftBool == rightBool
+                PyTokenTypes.NE -> leftBool != rightBool
                 else -> null
             }
+        }
 
-        private val PyExpression.bigInt: BigInteger?
-            get() = when (this) {
-                is PyNumericLiteralExpression -> this.bigIntegerValue
-                is PyBinaryExpression -> bigInt
-                is PyParenthesizedExpression -> containedExpression?.bigInt
-                else -> null
-            }
-
-        private val PyBinaryExpression.bool: Boolean?
-            get() {
-                val leftBigInt = leftExpression?.bigInt
-                val rightBigInt = rightExpression?.bigInt
-                if (leftBigInt != null && rightBigInt != null) {
-                    return when (operator) {
-                        PyTokenTypes.LT -> leftBigInt < rightBigInt
-                        PyTokenTypes.GT -> leftBigInt > rightBigInt
-                        PyTokenTypes.LE -> leftBigInt <= rightBigInt
-                        PyTokenTypes.GE -> leftBigInt >= rightBigInt
-                        PyTokenTypes.EQEQ -> leftBigInt == rightBigInt
-                        PyTokenTypes.NE -> leftBigInt != rightBigInt
-                        else -> null
-                    }
-                }
-
-                val leftBool = leftExpression?.bool
-                val rightBool = rightExpression?.bool
-                if (leftBool != null && rightBool != null) {
-                    return when (operator) {
-                        PyTokenTypes.AND_KEYWORD -> leftBool && rightBool
-                        PyTokenTypes.OR_KEYWORD -> leftBool || rightBool
-                        PyTokenTypes.EQEQ -> leftBool == rightBool
-                        PyTokenTypes.NE -> leftBool != rightBool
-                        else -> null
-                    }
-                }
-
-                return null
-            }
-
-        private val PyBinaryExpression.bigInt: BigInteger?
-            get() {
-                val leftBigInt = leftExpression?.bigInt ?: return null
-                val rightBigInt = rightExpression?.bigInt ?: return null
-                return when (operator) {
-                    PyTokenTypes.PLUS -> leftBigInt + rightBigInt
-                    PyTokenTypes.MINUS -> leftBigInt - rightBigInt
-                    PyTokenTypes.MULT -> leftBigInt * rightBigInt
-                    PyTokenTypes.EXP -> leftBigInt.pow(rightBigInt.toInt())  // overflow
-                    PyTokenTypes.DIV -> if (rightBigInt != BigInteger.ZERO) (leftBigInt / rightBigInt) else null
-                    PyTokenTypes.PERC -> if (rightBigInt != BigInteger.ZERO) (leftBigInt % rightBigInt) else null
-                    else -> null
-                }
-            }
-
-        private val PyPrefixExpression.bool: Boolean?
-            get() {
-                val operand = operand?.bool ?: return null
-                return when (operator) {
-                    PyTokenTypes.NOT_KEYWORD -> !operand
-                    else -> null
-                }
-            }
+        return null
     }
-}
+
+private val PyBinaryExpression.bigInt: BigInteger?
+    get() {
+        val leftBigInt = leftExpression?.bigInt ?: return null
+        val rightBigInt = rightExpression?.bigInt ?: return null
+        return when (operator) {
+            PyTokenTypes.PLUS -> leftBigInt + rightBigInt
+            PyTokenTypes.MINUS -> leftBigInt - rightBigInt
+            PyTokenTypes.MULT -> leftBigInt * rightBigInt
+            PyTokenTypes.EXP -> leftBigInt.pow(rightBigInt.toInt())  // overflow
+            PyTokenTypes.DIV -> if (rightBigInt != BigInteger.ZERO) (leftBigInt / rightBigInt) else null
+            PyTokenTypes.PERC -> if (rightBigInt != BigInteger.ZERO) (leftBigInt % rightBigInt) else null
+            else -> null
+        }
+    }
+
+private val PyPrefixExpression.bool: Boolean?
+    get() {
+        val operand = operand?.bool ?: return null
+        return when (operator) {
+            PyTokenTypes.NOT_KEYWORD -> !operand
+            else -> null
+        }
+    }
---
+++ src/main/kotlin/com/jetbrains/python/inspection/PyEquations.kt
@@ -0,0 +1,211 @@
+package com.jetbrains.python.inspection
+
+import com.jetbrains.python.PyTokenTypes
+import com.jetbrains.python.psi.*
+
+internal interface Equation {
+    fun not(): Equation
+    fun merge(other: Equation): Equation
+    fun canSolve(): Boolean
+}
+
+private interface VariableRestriction : Equation {
+    val varName: String
+}
+
+private data class EqEquation(override val varName: String, val value: Int) : VariableRestriction {
+
+    override fun not(): Equation = NotEqEquation(varName, value)
+
+    override fun merge(other: Equation): Equation {
+        if (other is VariableRestriction && varName != other.varName) return AndEquation(this, other)
+        return when (other) {
+            is EqEquation -> if (value == other.value) this else FailEquation
+            is NotEqEquation -> if (value != other.value) this else FailEquation
+            is LowerEqualEquation -> if (value <= other.bound) this else FailEquation
+            is GreaterEqualEquation -> if (value >= other.bound) this else FailEquation
+            is AndEquation -> other.equations.fold(this as Equation) { acc, next -> acc.merge(next) }
+            is OrEquation -> OrEquation(other.equations.map(this::merge).toSet())
+            is OkEquation -> this
+            else -> FailEquation
+        }
+    }
+
+    override fun canSolve(): Boolean = true
+}
+
+private data class NotEqEquation(override val varName: String, val value: Int) : VariableRestriction {
+
+    override fun not(): Equation = EqEquation(varName, value)
+
+    override fun merge(other: Equation): Equation {
+        if (other is VariableRestriction && varName != other.varName) return AndEquation(this, other)
+        return when (other) {
+            is EqEquation -> if (value != other.value) other else FailEquation
+            is NotEqEquation -> if (value == other.value) this else AndEquation(this, other)
+            is LowerEqualEquation -> if (value <= other.bound) AndEquation(this, other) else other
+            is GreaterEqualEquation -> if (value >= other.bound) AndEquation(this, other) else other
+            is AndEquation -> other.equations.fold(this as Equation) { acc, next -> acc.merge(next) }
+            is OrEquation -> OrEquation(other.equations.map(this::merge).toSet())
+            is OkEquation -> this
+            else -> FailEquation
+        }
+    }
+
+    override fun canSolve(): Boolean = true
+}
+
+private data class LowerEqualEquation(override val varName: String, val bound: Int) : VariableRestriction {
+
+    override fun not(): Equation = GreaterEqualEquation(varName, bound - 1)
+
+    override fun merge(other: Equation): Equation {
+        if (other is VariableRestriction && varName != other.varName) return AndEquation(this, other)
+        return when (other) {
+            is EqEquation -> if (other.value <= bound) other else FailEquation
+            is NotEqEquation -> if (other.value <= bound) AndEquation(this, other) else this
+            is LowerEqualEquation -> if (bound <= other.bound) this else other
+            is GreaterEqualEquation -> if (bound >= other.bound) AndEquation(this, other) else FailEquation
+            is AndEquation -> other.equations.fold(this as Equation) { acc, next -> acc.merge(next) }
+            is OrEquation -> OrEquation(other.equations.map(this::merge).toSet())
+            is OkEquation -> this
+            else -> FailEquation
+        }
+    }
+
+    override fun canSolve(): Boolean = true
+}
+
+private data class GreaterEqualEquation(override val varName: String, val bound: Int) : VariableRestriction {
+
+    override fun not(): Equation = LowerEqualEquation(varName, bound + 1)
+
+    override fun merge(other: Equation): Equation {
+        if (other is VariableRestriction && varName != other.varName) return AndEquation(this, other)
+        return when (other) {
+            is EqEquation -> if (other.value >= bound) other else FailEquation
+            is NotEqEquation -> if (other.value >= bound) AndEquation(this, other) else this
+            is LowerEqualEquation -> if (bound <= other.bound) AndEquation(this, other) else FailEquation
+            is GreaterEqualEquation -> if (bound >= other.bound) this else other
+            is AndEquation -> other.equations.fold(this as Equation) { acc, next -> acc.merge(next) }
+            is OrEquation -> OrEquation(other.equations.map(this::merge).toSet())
+            is OkEquation -> this
+            else -> FailEquation
+        }
+    }
+
+    override fun canSolve(): Boolean = true
+}
+
+private data class AndEquation(val equations: Set<Equation>) : Equation {
+
+    constructor(vararg equations: Equation): this(equations.toSet())
+
+    override fun not(): Equation = OrEquation(equations.map { it.not() }.toSet())
+
+    override fun merge(other: Equation): Equation = when (other) {
+        is EqEquation -> other.merge(this)
+        is NotEqEquation -> other.merge(this)
+        is LowerEqualEquation -> other.merge(this)
+        is GreaterEqualEquation -> other.merge(this)
+        is AndEquation -> other.equations.fold(this as Equation) { acc, next -> acc.merge(next) }
+        is OrEquation -> OrEquation(other.equations.map(this::merge).toSet())
+        is OkEquation -> this
+        else -> FailEquation
+    }
+
+    override fun canSolve(): Boolean = equations.all { it.canSolve() }
+}
+
+private data class OrEquation(val equations: Set<Equation>) : Equation {
+
+    constructor(vararg equations: Equation): this(equations.toSet())
+
+    override fun not(): Equation = AndEquation(equations.map { it.not() }.toSet())
+
+    override fun merge(other: Equation): Equation = when (other) {
+        is EqEquation -> other.merge(this)
+        is NotEqEquation -> other.merge(this)
+        is LowerEqualEquation -> other.merge(this)
+        is GreaterEqualEquation -> other.merge(this)
+        is AndEquation -> other.merge(this)
+        is OrEquation -> OrEquation(equations + other.equations)
+        is OkEquation -> this
+        else -> FailEquation
+    }
+
+    override fun canSolve(): Boolean = equations.any { it.canSolve() }
+}
+
+private object OkEquation : Equation {
+    override fun not(): Equation = FailEquation
+    override fun merge(other: Equation): Equation = other
+    override fun canSolve(): Boolean = true
+}
+
+private object FailEquation : Equation {
+    override fun not(): Equation = OkEquation
+    override fun merge(other: Equation): Equation = FailEquation
+    override fun canSolve(): Boolean = false
+}
+
+internal val PyExpression.equation: Equation
+    get() = when (this) {
+        is PyBinaryExpression -> equation
+        is PyPrefixExpression -> equation
+        is PyParenthesizedExpression -> equation
+        else -> OkEquation
+    }
+
+private val PyBinaryExpression.equation: Equation
+    get() {
+        if (leftExpression is PyReferenceExpression && rightExpression is PyReferenceExpression) {
+            return OkEquation  // TODO
+        } else if (leftExpression is PyReferenceExpression) {
+            val name = leftExpression.name
+            val value = rightExpression?.bigInt?.toInt()
+            if (name == null || value == null) return OkEquation
+            return when (operator) {
+                PyTokenTypes.LT -> LowerEqualEquation(name, value - 1)
+                PyTokenTypes.GT -> GreaterEqualEquation(name, value + 1)
+                PyTokenTypes.LE -> LowerEqualEquation(name, value)
+                PyTokenTypes.GE -> GreaterEqualEquation(name, value)
+                PyTokenTypes.EQEQ -> EqEquation(name, value)
+                PyTokenTypes.NE -> NotEqEquation(name, value)
+                else -> OkEquation
+            }
+        } else if (rightExpression is PyReferenceExpression) {
+            val name = rightExpression?.name
+            val value = leftExpression.bigInt?.toInt()
+            if (name == null || value == null) return OkEquation
+            return when (operator) {
+                PyTokenTypes.LT -> GreaterEqualEquation(name, value + 1)
+                PyTokenTypes.GT -> LowerEqualEquation(name, value - 1)
+                PyTokenTypes.LE -> GreaterEqualEquation(name, value)
+                PyTokenTypes.GE -> LowerEqualEquation(name, value)
+                PyTokenTypes.EQEQ -> EqEquation(name, value)
+                PyTokenTypes.NE -> NotEqEquation(name, value)
+                else -> OkEquation
+            }
+        }
+
+        val leftEquation = leftExpression.equation
+        val rightEquation = rightExpression?.equation ?: return OkEquation
+        return when (operator) {
+            PyTokenTypes.AND_KEYWORD -> leftEquation.merge(rightEquation)
+            PyTokenTypes.OR_KEYWORD -> OrEquation(leftEquation, rightEquation)
+            else -> OkEquation
+        }
+    }
+
+private val PyPrefixExpression.equation: Equation
+    get() {
+        val operand = operand?.equation ?: return OkEquation
+        return when (operator) {
+            PyTokenTypes.NOT_KEYWORD -> operand.not()
+            else -> OkEquation
+        }
+    }
+
+private val PyParenthesizedExpression.equation: Equation
+    get() = containedExpression?.equation ?: OkEquation
